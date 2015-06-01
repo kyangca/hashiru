479ddb39df9d27ef0901f503ad68229e9dd4afb2
Kevin Yang <kyang3@caltech.edu>
2015-06-01 13:40:54 -0700

    Added GPU Implementation
    Added the scaffolding code, kernel call, and kernel for the
      GPU implementation of the hash cracking.  I foresee three
      remaining tasks before we can start testing:
    
      1. Write CPU hash function
      2. Write GPU hash function
      3. Figure out how to compare hash strings in GPU kernel.

--- hashiru_cuda.cu
+++ hashiru_cuda.cu
@@ -1,5 +1,34 @@
 #include <cstdio>
+#include <string.h>
 #include <cuda_runtime.h>
 #include "hashiru_cuda.cuh"
 
 // TODO: Write kernels
+__global__ void cudaCrackHashKernel(char *dict, const int max_length, const int dict_size, const char *to_crack, int *correct_idx)
+{
+    unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    char *current, *cur_hash;
+    while(idx < dict_size)
+    {
+        current = dict + idx * max_length;
+        //TODO: cur_hash = salsa20_gpu(current);
+        //TODO: if(strcmp(cur_hash, to_crack) == 0)
+        if(0)
+        {
+            *correct_idx = idx;
+            break;
+        }
+        idx += blockDim.x * gridDim.x;
+    }
+}
+
+void cudaCallCrackHashKernel(const unsigned int blocks,
+         const unsigned int threadsPerBlock,
+         char *dict,
+         const int max_length,
+         const int dict_size,
+         const char *to_crack,
+         int *correct_idx)
+{
+    // TODO:  cudaCrackHashKernel<<<blocks, threadsPerBlock>>>(dict, max_length, dict_size, to_crack, correct_idx);
+}
--- hashiru_cuda.cuh
+++ hashiru_cuda.cuh
@@ -3,6 +3,12 @@
 
 
 // TODO: Write.
-
+void cudaCallCrackHashKernel(const unsigned int blocks,
+         const unsigned int threadsPerBlock,
+         char *dict,
+         const int max_length,
+         const int dict_size,
+         const char *to_crack,
+         int *correct_idx);
 
 #endif // CUDA_HASHIRU_CUH
